002befd42ee1c5680fb279b23029cab287b158ff
AACHAL2303PATIL <143063155+AACHAL2303PATIL@users.noreply.github.com>
2024-07-27 12:58:38 +0530

    feat: moved router code from main.jsx to rounter/index.jsx (#226)

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { RouterProvider } from 'react-router-dom';
 import './global.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
-import Home from './pages/Home.jsx';
-import PageViews from './components/PageViews/PageViews.jsx';
-import Africa from './pages/Africa.jsx';
-import NotFound from './pages/NotFound.jsx';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-    {
-        path: '/permanent/:id',
-        element: <App />,
-    },
-    {
-        path: '/home',
-        element: <Home />,
-    },
-    {
-        path: '/page-views',
-        element: <PageViews />,
-    },
-    {
-        path: '/top-africa',
-        element: <Africa />,
-    },
-    {
-        path: '*',
-        element: <NotFound />,
-    },
-]);
+import router from '../routers/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Layout>
